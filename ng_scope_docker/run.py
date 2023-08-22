38b711a62e4b34d37e31b3a7491b714351c889d2
Alexander Marder <amarder@jhu.edu>
2023-08-22 01:29:49 -0400

    podman stuff and sib renaming

--- ng_scope_docker/run.py
+++ ng_scope_docker/run.py
@@ -40,6 +40,7 @@ def main():
     docker_cmd = f'{args.prog} run --name {args.name} -ti --privileged --rm -v {args.log}:/ng-scope/build/ngscope/src/logs/ {args.image}'
     exec_cmd = f'./ngscope > /dev/null; ./ngscope -c logs/config.cfg -s "logs/sibs_{freq}.dump" -o logs/dci_output/'
     cmd = f'{docker_cmd} {exec_cmd}'
+    print(cmd)
     try:
         result = sp.run(cmd, shell=True, timeout=timeout)
     except KeyboardInterrupt:
